767a3f8bc994a549ccbc23f842c199f8704afacc
Pastechn <62227537+Pastechn@users.noreply.github.com>
2021-02-04 00:13:52 +0800

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script by Pastech
+# version 1.0
+
+# Helloworld
+git clone https://github.com/fw876/helloworld
+mv helloworld/* openwrt/package/lean/
+
+cd openwrt/package/lean
+
+# Argon new version
+rm -r luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon
+
+# Hello world
+git clone https://github.com/jerrykuku/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb
